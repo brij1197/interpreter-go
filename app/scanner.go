91d5d178e1e2c1a2d585d04af1e6ed8d4c690b64
Brij B <brijbhatia997@gmail.com>
2025-03-25 17:10:29 -0700

    Added Brace

--- app/scanner.go
+++ app/scanner.go
@@ -3,17 +3,23 @@ package main
 import "fmt"
 
 const (
-	LEFT_PAREN rune = '('
+	LEFT_PAREN  rune = '('
 	RIGHT_PAREN rune = ')'
+	LEFT_BRACE  rune = '{'
+	RIGHT_BRACE rune = '}'
 )
 
-func scanTokens(fileContents string){
-	for _, current := range fileContents{
+func scanTokens(fileContents string) {
+	for _, current := range fileContents {
 		switch current {
-			case LEFT_PAREN:
-				fmt.Println("LEFT_PAREN ( null")
-			case RIGHT_PAREN:
-				fmt.Println("RIGHT_PAREN ) null")
+		case LEFT_PAREN:
+			fmt.Println("LEFT_PAREN ( null")
+		case RIGHT_PAREN:
+			fmt.Println("RIGHT_PAREN ) null")
+		case LEFT_BRACE:
+			fmt.Println("LEFT_BRACE { null")
+		case RIGHT_BRACE:
+			fmt.Println("RIGHT_BRACE } null")
 		}
 	}
 	fmt.Println("EOF  null")
