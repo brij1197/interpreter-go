1025c95da79a34d4229b978f597ab8b849000815
Brij Bhatia <brijbhatia997@gmail.com>
2025-04-05 00:11:01 -0700

    Runtime Errors: Binary Operators 1

--- app/interpreter.go
+++ app/interpreter.go
@@ -55,6 +55,34 @@ func (i *Interpreter) VisitBinaryExpr(expr *Binary) interface{} {
 	right := i.Evaluate(expr.Right)
 
 	switch expr.Operator.Type {
+	case STAR:
+		if _, ok := left.(float64); !ok {
+			panic(&RuntimeError{
+				token:   expr.Operator,
+				message: "Operands must be numbers.",
+			})
+		}
+		if _, ok := right.(float64); !ok {
+			panic(&RuntimeError{
+				token:   expr.Operator,
+				message: "Operands must be numbers.",
+			})
+		}
+		return left.(float64) * right.(float64)
+	case SLASH:
+		if _, ok := left.(float64); !ok {
+			panic(&RuntimeError{
+				token:   expr.Operator,
+				message: "Operands must be numbers.",
+			})
+		}
+		if _, ok := right.(float64); !ok {
+			panic(&RuntimeError{
+				token:   expr.Operator,
+				message: "Operands must be numbers.",
+			})
+		}
+		return left.(float64) / right.(float64)
 	case EQUAL_EQUAL:
 		return i.isEqual(left, right)
 	case BANG_EQUAL:
@@ -98,18 +126,6 @@ func (i *Interpreter) VisitBinaryExpr(expr *Binary) interface{} {
 		if l, ok := left.(float64); ok {
 			if r, ok := right.(float64); ok {
 				return l - r
-			}
-		}
-	case STAR:
-		if l, ok := left.(float64); ok {
-			if r, ok := right.(float64); ok {
-				return l * r
-			}
-		}
-	case SLASH:
-		if l, ok := left.(float64); ok {
-			if r, ok := right.(float64); ok {
-				return l / r
 			}
 		}
 	}
