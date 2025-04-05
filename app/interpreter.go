665df65936ada79f9c7745431a523e44d7d92e10
Brij Bhatia <brijbhatia997@gmail.com>
2025-04-04 22:55:10 -0700

    Runtime Errors: Unary Operators

--- app/interpreter.go
+++ app/interpreter.go
@@ -1,9 +1,23 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 type Interpreter struct{}
+
+type RuntimeError struct {
+	token   Token
+	message string
+}
 
 func NewInterpreter() *Interpreter {
 	return &Interpreter{}
+}
+
+func (e *RuntimeError) Error() string {
+	return fmt.Sprintf("%s\n[line %d]", e.message, e.token.Line)
 }
 
 func (i *Interpreter) Evaluate(expr Expr) interface{} {
@@ -22,10 +36,13 @@ func (i *Interpreter) VisitUnaryExpr(expr *Unary) interface{} {
 	right := i.Evaluate(expr.Right)
 	switch expr.Operator.Type {
 	case MINUS:
-		if num, ok := right.(float64); ok {
-			return -num
+		if _, ok := right.(float64); !ok {
+			panic(&RuntimeError{
+				token:   expr.Operator,
+				message: "Operand must be a number.",
+			})
 		}
-		return nil
+		return -(right.(float64))
 	case BANG:
 		return !i.isTruthy(right)
 	}
@@ -98,6 +115,23 @@ func (i *Interpreter) VisitBinaryExpr(expr *Binary) interface{} {
 	return nil
 }
 
+func (i *Interpreter) Interpret(expr Expr) {
+	defer func() {
+		if r := recover(); r != nil {
+			if runtimeErr, ok := r.(*RuntimeError); ok {
+				fmt.Fprintln(os.Stderr, runtimeErr.Error())
+				os.Exit(70)
+			} else {
+				panic(r)
+			}
+		}
+	}()
+	result := i.Evaluate(expr)
+	if result != nil {
+		fmt.Println(i.stringify(result))
+	}
+}
+
 func (i *Interpreter) isEqual(left, right interface{}) bool {
 	if left == nil && right == nil {
 		return true
@@ -134,4 +168,22 @@ func (i *Interpreter) isTruthy(object interface{}) bool {
 		return b
 	}
 	return true
+}
+
+func (i *Interpreter) stringify(value interface{}) string {
+	if value == nil {
+		return "nil"
+	}
+
+	switch v := value.(type) {
+	case float64:
+		text := fmt.Sprintf("%g", v)
+		return text
+	case string:
+		return v
+	case bool:
+		return fmt.Sprintf("%t", v)
+	default:
+		return fmt.Sprintf("%v", v)
+	}
 }
